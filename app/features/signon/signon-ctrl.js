968bbd29b64279f42c41afecd030284203318a48
Buddy Williams <buddywilliams@gmail.com>
2015-10-28 23:24:51 -0400

    basic error and status handling

--- app/features/signon/signon-ctrl.js
+++ app/features/signon/signon-ctrl.js
@@ -4,15 +4,30 @@ app.controller('SignOnCtrl', function($scope, $http, Config) {
 
   $scope.email = 'admin@torbit.com';
   $scope.password = 'torbit';
+  $scope.errorMessage = '';
+  $scope.statusMessage = '';
 
   $scope.signon = function() {
+    $scope.statusMessage = 'Loading...';
     $http.post(Config.serverUrl + '/login', { email: $scope.email, password: $scope.password })
       .then(function(httpData) {
         console.log(httpData);
       },
       function(httpData) {
         console.error(httpData);
+        $scope.errorMessage = 'Invalid email or password.';
+      })
+      .finally(function() {
+        $scope.statusMessage = '';
       });
   };
+
+  $scope.$watch('email', function() {
+    $scope.errorMessage = '';
+  });
+
+  $scope.$watch('password', function() {
+    $scope.errorMessage = '';
+  });
 
 });
