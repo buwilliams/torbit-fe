1aee1af54ee895c6a3754828274aa34019453f09
Buddy Williams <buddywilliams@gmail.com>
2015-11-01 11:44:23 -0500

    removed old code

--- app/features/home/home-ctrl.js
+++ app/features/home/home-ctrl.js
@@ -1,14 +1,4 @@
 var app = angular.module('torbitFeApp');
 
 app.controller('HomeCtrl', function($scope, $http, Config, User) {
-
-  $scope.getUsers = function() {
-    $http.get(Config.serverUrl + '/users')
-      .then(function(httpData) {
-        console.log('users', httpData);
-      }, function(httpData) {
-        console.error('users', httpData);
-      });
-  };
-
 });
