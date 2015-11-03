4fbbeeee79f8d92acc3b813f4aacfe41d63d1f02
Buddy Williams <buddywilliams@gmail.com>
2015-11-03 11:22:20 -0500

    added additional state so that we can cancel edits and the model returns to the original model;

--- app/features/users/users-ctrl.js
+++ app/features/users/users-ctrl.js
@@ -1,19 +1,45 @@
 var app = angular.module('torbitFeApp');
 
-app.controller('UsersCtrl', function($scope, UsersFactory) {
+app.controller('UsersCtrl', function($scope, $u, UsersFactory) {
 
-  UsersFactory.getUsers();
-  $scope.users = UsersFactory.data.users;
+  $scope.users = [];
+  $scope.newUser = {};
 
-  $scope.editUser = function() {
+  $scope.init = function() {
+    $scope.refresh();
+  };
+
+  $scope.save = function(form, row) {
+    if(form.$invalid) { return; } // don't submit form if there are validation errors
+    console.log('form submitted', form.$error, form, row);
+    return;
+    //UsersFactory.updateUser(row);
+  };
+
+  $scope.cancel = function(row) {
+    var originalRow = $u.find(UsersFactory.data.users, { email: row.email });
+    if(!_.isUndefined(originalRow)) {
+      $u.overwrite(row, originalRow);
+    } else {
+      // email wasn't found so assume the email address was changed
+    }
+  };
+
+  $scope.cancelNewUser = function() {
+    $u.empty($scope.newUser);
   };
 
   $scope.refresh = function() {
-    UsersFactory.getUsers();
+    UsersFactory.getUsers(function() {
+      var clonedUsers = angular.copy(UsersFactory.data.users);
+      $u.overwrite($scope.users, clonedUsers);
+    });
   };
 
   $scope.boolToStr = function(bool) {
     return bool ? 'true' : 'false';
   };
+
+  (function() { $scope.init(); })();
 
 });
