bff99501d230b81ed95305e419c6b50beddeecb5
Buddy Williams <buddywilliams@gmail.com>
2015-11-02 21:49:40 -0500

    added updateUser and deleteUser methods to the users-factory and their tests; more testing is needed around these apis;

--- app/features/users/users-factory.js
+++ app/features/users/users-factory.js
@@ -7,10 +7,41 @@ app.factory('UsersFactory', function($http, Config, $u) {
     'users': []
   };
 
+  // Gets the full list of users
   factory.getUsers = function(successFn, errorFn) {
     $http.get(Config.serverUrl + '/users')
       .then(function(httpData) {
         $u.overwrite(factory.data.users, httpData.data);
+        if(!_.isUndefined(successFn)) {
+          successFn(httpData.data);
+        }
+      }, function(httpData) {
+        if(!_.isUndefined(successFn)) {
+          errorFn(httpData);
+        }
+      });
+  };
+
+  // Serves for editing and creating users
+  factory.updateUser = function(user, successFn, errorFn) {
+    $http.post(Config.serverUrl + '/user', user)
+      .then(function(httpData) {
+        if(!_.isUndefined(successFn)) {
+          successFn(httpData.data);
+        }
+      }, function(httpData) {
+        if(!_.isUndefined(successFn)) {
+          errorFn(httpData);
+        }
+      });
+  };
+
+  factory.deleteUser = function(email, successFn, errorFn) {
+    var params = {
+      email: email
+    };
+    $http.delete(Config.serverUrl + '/user', { params: params })
+      .then(function(httpData) {
         if(!_.isUndefined(successFn)) {
           successFn(httpData.data);
         }
--- test/features/users/users-factory-spec.js
+++ test/features/users/users-factory-spec.js
@@ -63,4 +63,78 @@ describe('UsersFactory', function() {
 
   });
 
+  describe('updateUser', function() {
+
+    var fixture, userRequestHandler;
+
+    beforeEach(inject(function(_fixtureUsers_) {
+      fixture = _fixtureUsers_;
+      userRequestHandler = $httpBackend.when('POST',
+        Config.serverUrl + '/user').respond(fixture);
+    }));
+
+    it('should make the call to /user api as POST', function() {
+      $httpBackend.expectPOST(Config.serverUrl + '/user');
+      Factory.updateUser({});
+      $httpBackend.flush();
+    });
+
+    it('should invoke success fn', function() {
+      var successFn = jasmine.createSpy();
+      $httpBackend.expectPOST(Config.serverUrl + '/user');
+      Factory.updateUser({}, successFn);
+      $httpBackend.flush();
+      expect(successFn).toHaveBeenCalled();
+    });
+
+    it('should invoke error fn', function() {
+      userRequestHandler.respond(500);
+      var successFn = jasmine.createSpy();
+      var errorFn = jasmine.createSpy();
+      $httpBackend.expectPOST(Config.serverUrl + '/user');
+      Factory.updateUser({}, successFn, errorFn);
+      $httpBackend.flush();
+      expect(successFn).not.toHaveBeenCalled();
+      expect(errorFn).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('deleteUser', function() {
+
+    var fixture, userRequestHandler, emailAddress = 'test@test.com';
+
+    beforeEach(inject(function(_fixtureUsers_) {
+      fixture = _fixtureUsers_;
+      userRequestHandler = $httpBackend.when('DELETE',
+        Config.serverUrl + '/user?email='+emailAddress).respond(fixture);
+    }));
+
+    it('should make the call to /user api as DELETE', function() {
+      $httpBackend.expectDELETE(Config.serverUrl + '/user?email='+emailAddress);
+      Factory.deleteUser(emailAddress);
+      $httpBackend.flush();
+    });
+
+    it('should invoke success fn', function() {
+      var successFn = jasmine.createSpy();
+      $httpBackend.expectDELETE(Config.serverUrl + '/user?email='+emailAddress);
+      Factory.deleteUser(emailAddress, successFn);
+      $httpBackend.flush();
+      expect(successFn).toHaveBeenCalled();
+    });
+
+    it('should invoke error fn', function() {
+      userRequestHandler.respond(500);
+      var successFn = jasmine.createSpy();
+      var errorFn = jasmine.createSpy();
+      $httpBackend.expectDELETE(Config.serverUrl + '/user?email='+emailAddress);
+      Factory.deleteUser(emailAddress, successFn, errorFn);
+      $httpBackend.flush();
+      expect(successFn).not.toHaveBeenCalled();
+      expect(errorFn).toHaveBeenCalled();
+    });
+
+  });
+
 });
