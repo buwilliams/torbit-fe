e438264ff76597a3c6157f508551344c62986743
Buddy Williams <buddywilliams@gmail.com>
2015-11-02 21:53:45 -0500

    WIP, first pass at editing the table (cancel doesn't work properly becuase it doesn't revert the changes made while editing; WIP, also added first pass at UI for creating a new user; Refresh button works properly;

--- app/features/users/users-ctrl.js
+++ app/features/users/users-ctrl.js
@@ -5,4 +5,15 @@ app.controller('UsersCtrl', function($scope, UsersFactory) {
   UsersFactory.getUsers();
   $scope.users = UsersFactory.data.users;
 
+  $scope.editUser = function() {
+  };
+
+  $scope.refresh = function() {
+    UsersFactory.getUsers();
+  };
+
+  $scope.boolToStr = function(bool) {
+    return bool ? 'true' : 'false';
+  };
+
 });
